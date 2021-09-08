e224410383a0d499f6481220883d486aa2e655b3
Charlie <s099064146@gmail.com>
2021-09-08 11:44:03 +0800

    getting started finished

---
+++ carved-rock-fitness/home.js
@@ -0,0 +1,7 @@
+let welcome = "Welcome",
+    price = 49.99,
+    name = 'Hiking Boots',
+    discounted = false;
+
+showMessage(welcome);
+changePercentOff( 32 );
---
+++ carved-rock-fitness/utils.js
@@ -0,0 +1,11 @@
+function showMessage(message) {
+    document.getElementById('message').textContent = message;
+}
+
+function changePercentOff(percentage) {
+    document.getElementById('percent-off').textContent = percentage + "% OFF";
+}
+    
+
+
+
